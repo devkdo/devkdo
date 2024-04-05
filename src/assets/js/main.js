246203b23bf3fe0e721c441e41b7be2ea2d9e57f
devkdo <dekaido@outlook.com>
2024-04-05 00:42:31 -0400

    add navbar and colors

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+document.getElementsByName("logo").forEach((img) => {
+    img.style.filter="invert(100%)";
+});
+// document.getElementsByTagName("body").innerHTML = `
+
+// `;
